01f1cc274baa792e12ab3397660d1264e6bd2f02
Sophie Gosia <sophie.gosia@gmail.com>
2018-03-21 20:58:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,3 @@ add.addEventListener('click', function(e) {
 	list.appendChild(element);
 
   });
-
-  /* In this exercise HTML <li> elements should start with an "Item 0" number. 
-  I've changed it into "Item 1" because .length starts counting from 1 so after first click it adds a new button "item 2".
-  I have no idea, how author did it so that it counts buttons from 0. */
